a71c7aba66ab920786db0263fd9f8a433469c6fe
jean costa <costajean1005@gmail.com>
2025-06-18 14:43:42 -0300

    botões de excluir e copia

--- js/scripts.js
+++ js/scripts.js
@@ -56,11 +56,25 @@ function createNote(id, content, fixed) {
 
     element.appendChild(textarea);
 
-    const pinIcon = document.createElement("i")
+    const pinIcon = document.createElement("i");
 
     pinIcon.classList.add(...["bi" , "bi-pin"]);
 
     element.appendChild(pinIcon);
+
+    const deleteIcon = document.createElement("i");
+
+    deleteIcon.classList.add(...["bi" , "bi-x-lg"]);
+
+    element.appendChild(deleteIcon);
+
+    const duplicateIcon = document.createElement("i");
+
+    duplicateIcon.classList.add(...["bi" , "bi-file-earmark-plus"]);
+
+    element.appendChild(duplicateIcon);
+
+
 
     if(fixed){
         element.classList.add("fixed");
@@ -70,6 +84,14 @@ function createNote(id, content, fixed) {
 
     element.querySelector(".bi-pin").addEventListener("click", () => {
         toggleFixNote(id);
+    });
+
+    element.querySelector(".bi-x-lg").addEventListener("click", () => {
+        deleteNote(id, element);
+    });
+
+    element.querySelector(".bi-file-earmark-plus").addEventListener("click", () => {
+        copyNote(id);
     });
 
     return element;
@@ -87,12 +109,38 @@ function toggleFixNote(id){
     showNotes();
 }
 
+function deleteNote(id, element){
+    const notes = getNotes(). filter((note) => note.id !== id);
+
+    saveNotes(notes);
+    notesContainer.removeChild(element);
+}
+
+function copyNote(id){
+    const notes = getNotes();
+
+    const targetNote = notes.filter((note) => note.id === id)[0];
+
+    const noteObject = {
+        id: generateId(),
+        content: targetNote.content,
+        fixed: false,
+    };
+const noteElement = createNote(noteObject.id, noteObject.content, noteObject.fixed);
+
+notesContainer.appendChild(noteElement);
+
+notes.push(noteObject);
+
+saveNotes(notes);
+
+}
 
 // Local storange
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem("notes") || "[}");
 
-    const orderedNotes = notes.sort((a, b) => (a.fixed . b.fixed ? -1 : 1));
+    const orderedNotes = notes.sort((a, b) => (a.fixed > b.fixed ? -1 : 1));
     return orderedNotes;
 }
 function saveNotes(notes){
